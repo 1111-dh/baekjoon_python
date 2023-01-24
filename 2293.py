eabb7884b9296d5d1bddae9b1b6c6b4816858358
Jdeokhwan <ghjnm1289@naver.com>
2023-01-24 23:58:06 +0900

    230124

--- 2293.py
+++ 2293.py
@@ -4,8 +4,10 @@ input=sys.stdin.readline
 n,k=map(int,input().split())
 coin=[int(input()) for _ in range(n)]
 DP=[0]*(k+1)
-for i in range(1,k+1):
-    for j in coin:
-        if i-j>=0:
-            DP[i]+=DP[i-j]+1
-print(DP)
+DP[0]=1
+
+for i in coin:
+    for j in range(i,k+1):
+        if j-i>=0:
+            DP[j]+=DP[j-i]
+print(DP[-1])
