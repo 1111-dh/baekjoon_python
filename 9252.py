26c2a2a8e9f44f7cd659c97f60f1164ecbb318ce
Jdeokhwan <ghjnm1289@naver.com>
2023-02-06 22:19:06 +0900

    230206

--- 9252.py
+++ 9252.py
@@ -7,12 +7,22 @@ len_a=len(a)
 len_b=len(b)
 
 answer=[0]*len_a
+string=['']*len_a
 for i in range(len_b):
     cnt=0
+    tmp=''
     for j in range(len_a):
         if cnt<answer[j]:
             cnt=answer[j]
+            tmp=string[j]
         elif b[i]==a[j]:
             answer[j]=cnt+1
-        
-print(max(answer))
+            string[j]=tmp+a[j]
+max_len=0
+max_str=''
+for i in range(len_a):
+    if max_len<answer[i]:
+        max_len=answer[i]
+        max_str=string[i]
+print(max_len)
+print(max_str)
